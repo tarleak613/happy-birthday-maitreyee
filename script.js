1ed91f0ca95a82d4a02950dc83d9905c51ec5deb
tarleak <ayushbhagat1213@gmail.com>
2026-08-30 13:00:45 +0530

    birthday surprise for Maitreyee

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function hideLoadingOverlay() {
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const WEBCAM_WIDTH = isMobile ? 240 : 300;
 const WEBCAM_HEIGHT = isMobile ? 180 : 225;
-const BLOW_THRESHOLD = isMobile ? 35 : 65; // how sensitive the mic is
+const BLOW_THRESHOLD = isMobile ? 41 : 65; // how sensitive the mic is
 const LIGHT_DISTANCE = 20; // how close match needs to be to light candles
 
 canvas.width = WEBCAM_WIDTH;
